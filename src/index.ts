e6c75eadc272d1be5e9cb5f7239ead3f45362057
adriansyahdickyklickers <mochammad_dicky@klickersgroup.com>
2023-06-04 22:17:13 +0700

    post crud graphql

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,34 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from 'express'
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
     const orm = MikroORM.init(mikroOrmConfig);
     (await orm).getMigrator().up();
-    const post = (await orm).em.fork({}).create(Post, {
-        title: "joko",
-        createdAt: new Date(),
-        updatedAt: new Date(),
+    
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false
+        }),
+        context: async () => ({ em: (await orm).em })
     });
 
-    (await orm).em.persistAndFlush(post);
-    console.log('success insert');
-}
+    await apolloServer.start();
+    apolloServer.applyMiddleware({app});
+
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000');
+    });
+};
 
 main().catch(err => {
     console.error(err);
